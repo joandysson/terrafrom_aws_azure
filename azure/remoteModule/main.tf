c709d0bcc09a30cf75c9d0e8c24658a56eb72c28
root <root@Gama>
2022-12-18 16:22:24 -0300

    remote module

---
+++ azure/remoteModule/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "> 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.35.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "remote-state"
+    storage_account_name = "firststorageaccou"
+    container_name       = "images"
+    key                  = "azure-vm-remote-module/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  version             = "3.5.0"
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
--- azure/remoteModule/network.tf
+++ azure/remoteModule/network.tf
@@ -1,28 +1,3 @@
-resource "azurerm_resource_group" "resource_group" {
-  name     = "vnet"
-  location = var.location
-
-  tags = local.common_tags
-}
-
-resource "azurerm_virtual_network" "virtual_network" {
-  name                = "nvet-terraform"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  address_space       = ["10.0.0.0/16"]
-
-
-  tags = local.common_tags
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-terraform"
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-terraform"
   location            = var.location
@@ -45,6 +20,6 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "snsga" {
-  subnet_id                 = azurerm_subnet.subnet.id
+  subnet_id                 = module.network.vnet_subnets[0]
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
